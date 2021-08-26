6716110124a905f0b9d2e4efd197f7f024b6ef2e
ESSmirnova <katevaselkova@me.com>
2021-08-26 11:20:24 +0300

    update

--- src/test/java/example/LoginTest.java
+++ src/test/java/example/LoginTest.java
@@ -1,22 +1,19 @@
 package example;
 
-import org.junit.AfterClass;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import twitter.LoginPage;
 import utilites.Helper;
 
-import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
-//    public static LoginPage loginPage;
-//    public static ProfilePage profilePage;
+
     public static WebDriver driver;
     LoginPage loginPageObject;
 
@@ -43,10 +40,8 @@ public class LoginTest {
 //    }
 
     @AfterMethod
-    public void screenshotOnFailure(ITestResult result)
-    {
-        if (result.getStatus() == ITestResult.FAILURE)
-        {
+    public void screenshotOnFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
             System.out.println("Failed!");
             System.out.println("Taking Screenshot....");
             Helper.captureScreenshot(driver, result.getName());
